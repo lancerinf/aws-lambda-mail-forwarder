68d6ae0a197829be9bb2f6f6c4a29f1c2f6f6b05
Federico Lancerin <federico.lancerin@gmail.com>
2020-02-04 23:45:49 +0100

    add friendly name formatting From header

--- mail_forwarder/app.py
+++ mail_forwarder/app.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
         # Check if valid email
         email_message = parse_email_message(s3_object_bytes)
 
-        # Modify EmailMessage replyTo and From fields
+        # Modify EmailMessage Reply-To and From fields
         # Send new/modified EmailMessage object with SES
         ses_client = client('ses')
         send_result = twist_and_forward(ses_client, email_message)
@@ -35,8 +35,7 @@ def get_object_from_s3(s3_client, s3_notification):
     key = urllib.parse.unquote(s3_notification['s3']['object']['key'])
     s3_object_response = s3_client.get_object(
         Bucket=bucket,
-        # Key=key
-        Key='inbox/federico@lancerin.com/v2ehb4igsa1n6n9323oqvna4so3knlg6h0985f01'
+        Key=key
     )
     return s3_object_response['Body'].read()
 
@@ -51,12 +50,16 @@ def twist_and_forward(ses_client, email_message):
     forward_from = os.environ['FORWARD_FROM_ADDRESS']
     forward_to = os.environ['FORWARD_TO_ADDRESS']
 
+    original_from_header = email_message.get('From')
+    enriched_from_header = format_friendly_name(original_from_header, forward_from)
+
+    # Modify EmailMessage Headers
     if not email_message.get('Reply-To'):
-        email_message.add_header('Reply-To', email_message.get('From'))
-
+        email_message.add_header('Reply-To', original_from_header)
     del email_message['Return-Path']
-    email_message.replace_header('From', forward_from)
+    email_message.replace_header('From', enriched_from_header)
 
+    # Actually send the email
     send_result = ses_client.send_raw_email(
         Source=forward_from,
         Destinations=[
@@ -66,5 +69,10 @@ def twist_and_forward(ses_client, email_message):
             'Data': email_message.as_bytes()
         }
     )
-
     return send_result
+
+
+def format_friendly_name(email_from_header, forward_from):
+    email_from_header = email_from_header.replace('<', '~')
+    email_from_header = email_from_header.replace('>', '~')
+    return f'{email_from_header} <{forward_from}>'
