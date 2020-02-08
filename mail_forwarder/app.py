212cd577dcc6eec1c0af7d09ddb7ea99d8d57dc6
Federico Lancerin <federico.lancerin@gmail.com>
2020-02-08 16:05:34 +0100

    del DKIM headers, fix Reply-To in case Return-Path is present

--- mail_forwarder/app.py
+++ mail_forwarder/app.py
@@ -16,11 +16,13 @@ def lambda_handler(event, context):
         # Check if valid email
         email_message = parse_email_message(s3_object_bytes)
 
-        # Modify EmailMessage Reply-To and From fields
-        # Send new/modified EmailMessage object with SES
+        # Prepare EmailMessage to be forwarded, switch headers
+        email_message = prepare_email_for_forwarding(email_message)
+
+        # Send modified EmailMessage with SES
         ses_client = client('ses')
-        send_result = twist_and_forward(ses_client, email_message)
-        print(send_result)
+        send_result = forward_email(ses_client, email_message)
+        return send_result
 
     except s3_client.exceptions.NoSuchKey as e:
         print(f'S3Exception(NoSuchKey): {e}')
@@ -46,18 +48,33 @@ def parse_email_message(s3_object_bytes):
     return parsed_email_message
 
 
-def twist_and_forward(ses_client, email_message):
+def prepare_email_for_forwarding(email_message):
+    forward_from = os.environ['FORWARD_FROM_ADDRESS']
+    enriched_from_header = format_friendly_name(email_message['From'], forward_from)
+
+    # Ensure Reply-To header is set to most relevant Address.
+    if email_message['Return-Path']:
+        if not email_message['Reply-To']:
+            email_message.add_header('Reply-To', email_message['Return-Path'])
+        del email_message['Return-Path']
+
+    # Set From header to user-friendly name.
+    email_message.replace_header('From', enriched_from_header)
+
+    # Remove all pre-existing DKIM-Signature headers:
+    del email_message['DKIM-Signature']
+    return email_message
+
+
+def format_friendly_name(email_from_header, forward_from):
+    email_from_header = email_from_header.replace('<', '~')
+    email_from_header = email_from_header.replace('>', '~')
+    return f'{email_from_header} <{forward_from}>'
+
+
+def forward_email(ses_client, email_message):
     forward_from = os.environ['FORWARD_FROM_ADDRESS']
     forward_to = os.environ['FORWARD_TO_ADDRESS']
-
-    original_from_header = email_message.get('From')
-    enriched_from_header = format_friendly_name(original_from_header, forward_from)
-
-    # Modify EmailMessage Headers
-    if not email_message.get('Reply-To'):
-        email_message.add_header('Reply-To', original_from_header)
-    del email_message['Return-Path']
-    email_message.replace_header('From', enriched_from_header)
 
     # Actually send the email
     send_result = ses_client.send_raw_email(
@@ -70,9 +87,3 @@ def twist_and_forward(ses_client, email_message):
         }
     )
     return send_result
-
-
-def format_friendly_name(email_from_header, forward_from):
-    email_from_header = email_from_header.replace('<', '~')
-    email_from_header = email_from_header.replace('>', '~')
-    return f'{email_from_header} <{forward_from}>'
